916b203e18f1e6b8e1bf8f6493e0d6beefe376ae
Woozuu <wangh.ericfs@gmail.com>
2026-01-14 00:23:37 +0100

    Change pose model complexity from 2 to 1

--- Web_App_Squat_Assessment.py
+++ Web_App_Squat_Assessment.py
@@ -57,7 +57,7 @@ class SetSummary:
 
 class PoseProcessor:
     def __init__(self):
-        self.pose = mp_pose.Pose(model_complexity=2, min_detection_confidence=0.5, min_tracking_confidence=0.5)
+        self.pose = mp_pose.Pose(model_complexity=1, min_detection_confidence=0.5, min_tracking_confidence=0.5)
 
     def process_video(self, video_path: str):
         cap = cv2.VideoCapture(video_path)
