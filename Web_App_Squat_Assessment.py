4fa6d71dab0025f65bc8566a1ebdf581df9419d9
Woozuu <wangh.ericfs@gmail.com>
2026-01-14 00:58:01 +0100

    Refactor MediaPipe imports for clarity

--- Web_App_Squat_Assessment.py
+++ Web_App_Squat_Assessment.py
@@ -15,9 +15,9 @@ print(st.__version__)
 print(cv2.__version__)
 print(mp.__version__)
 # --- MEDIA PIPE UTILS ---
-mp_pose = mp.solutions.pose
-mp_drawing = mp.solutions.drawing_utils
-mp_drawing_styles = mp.solutions.drawing_styles
+from mediapipe.python.solutions import pose as mp_pose
+from mediapipe.python.solutions import drawing_utils as mp_drawing
+from mediapipe.python.solutions import drawing_styles as mp_drawing_styles
 
 # ==========================================
 # 1. DATA STRUCTURES (From original design)
